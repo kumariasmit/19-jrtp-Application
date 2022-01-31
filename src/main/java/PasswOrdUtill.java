78d155e35ee3ef0fd0c1c7f263492938b47dcc84
kumariasmit <kumarimunni0501@gmail.com>
2022-01-31 07:56:26 +0530

    bug fix102

--- src/main/java/PasswOrdUtill.java
+++ src/main/java/PasswOrdUtill.java
@@ -1,8 +1,19 @@
 
 public class PasswOrdUtill {
 	
+
 	public static void Hello() {
 		System.out.println("Hello");
+	}
+
+	public void encode() {
+		System.out.println("Helllo bug1");
+		System.out.println("Helllo bug2");
+	}
+
+	public void decode() {
+
+
 	}
 
 }
